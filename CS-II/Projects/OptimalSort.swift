2b83f1b96fd67f1880312fa74821ea3e79831fc2
berkley-voss <berkley.voss@student.allenisd.org>
2022-09-20 10:41:11 -0500

    Everything

---
+++ CS-II/Projects/OptimalSort.swift
@@ -0,0 +1,10 @@
+import Foundation
+import Swift
+
+let names = ["edfbdgbf" , "fgmnsgrt" , "wrhearhaer" , "fgmnsgart's"]
+let sortedNames = names.sorted { (lhs: String, rhs: String) -> Bool in
+    return lhs.caseInsensitiveCompare(rhs) == .orderedAscending
+}
+for name in sortedNames {
+        print(name)
+    }
